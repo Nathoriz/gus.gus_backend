0aae9b3f7d93d4f3f6a4c487d514407979ba36af
DeamBrose <emiliodragon98@hotmail.com>
2021-10-19 16:10:05 -0500

    Changed variable of entity Cliente (nombre Completo)

--- src/main/java/gustitodecasa/com/GustitoDeCasa_version_10/entity/Cliente.java
+++ src/main/java/gustitodecasa/com/GustitoDeCasa_version_10/entity/Cliente.java
@@ -13,8 +13,7 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nombre;
-    private String apellido;
+    private String nombreCompleto;
     private String direccion;
     private String telefono;
 }
