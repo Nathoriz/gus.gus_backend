1236e9a6d785293893c655381bd0633faf52250d
Your Name <chris.elramzes@gmail.com>
2021-10-19 19:25:50 -0500

    crud_entrega

---
+++ src/main/java/gustitodecasa/com/GustitoDeCasa_version_10/repository/EntregaRepository.java
@@ -0,0 +1,2 @@
+package gustitodecasa.com.GustitoDeCasa_version_10.repository;public interface EntregaRepository {
+}
--- src/main/java/gustitodecasa/com/GustitoDeCasa_version_10/service/EntregaService.java
+++ src/main/java/gustitodecasa/com/GustitoDeCasa_version_10/service/EntregaService.java
@@ -1,13 +1,2 @@
-package gustitodecasa.com.GustitoDeCasa_version_10.service;
-
-import gustitodecasa.com.GustitoDeCasa_version_10.entity.Entrega;
-
-import java.util.List;
-
-public interface EntregaService  {
-    public List<Entrega> findAll();
-    public void save (Entrega entre);
-    public Entrega findById(Long id);
-    public void delete (Entrega entre);
-
+package gustitodecasa.com.GustitoDeCasa_version_10.service;public class EntregaService {
 }
