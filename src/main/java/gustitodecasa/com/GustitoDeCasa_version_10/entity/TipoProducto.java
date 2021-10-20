d94f9593bc8525f29328e24769078b14c14eb299
Mod <natalyortiz.401@gmail.com>
2021-10-20 18:20:35 -0500

    version 2 (entity,repo,serv,contr) de TipoProducto

--- src/main/java/gustitodecasa/com/GustitoDeCasa_version_10/entity/TipoProducto.java
+++ src/main/java/gustitodecasa/com/GustitoDeCasa_version_10/entity/TipoProducto.java
@@ -3,6 +3,7 @@ package gustitodecasa.com.GustitoDeCasa_version_10.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Data
@@ -12,12 +13,12 @@ public class TipoProducto {
     private Long id;
     private String nombre;
     private String cubierta;
-    @ManyToOne
-    private Sabor sabor;
-    @ManyToOne
-    private Diametro diametro;
-    @ManyToOne
-    private Tamanio tamanio;
-    @ManyToOne
-    private Relleno relleno;
+    @OneToMany
+    private List<Sabor> listaSabores;
+    @OneToMany
+    private List<Diametro> listaDiametros;
+    @OneToMany
+    private List<Tamanio> listaTamanios;
+    @OneToMany
+    private List<Relleno> listaRellenos;
 }
--- src/main/java/gustitodecasa/com/GustitoDeCasa_version_10/repository/TamanioRepository.java
+++ src/main/java/gustitodecasa/com/GustitoDeCasa_version_10/repository/TamanioRepository.java
@@ -3,5 +3,7 @@ package gustitodecasa.com.GustitoDeCasa_version_10.repository;
 import gustitodecasa.com.GustitoDeCasa_version_10.entity.Tamanio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TamanioRepository extends JpaRepository<Tamanio, Long> {
 }
