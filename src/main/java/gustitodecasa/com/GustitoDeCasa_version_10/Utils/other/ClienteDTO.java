b08702da08b7cc1606c8fa5b36285f329fd85dd4
DeamBrose <emiliodragon98@hotmail.com>
2021-10-22 09:00:02 -0500

    Utils and entrega service

---
+++ src/main/java/gustitodecasa/com/GustitoDeCasa_version_10/Utils/other/ClienteDTO.java
@@ -0,0 +1,10 @@
+package gustitodecasa.com.GustitoDeCasa_version_10.Utils.other;
+
+import lombok.Data;
+
+@Data
+public class ClienteDTO {
+    private String nombreCompleto;
+    private String direccion;
+    private String telefono;
+}
